ce105060f3bc71805dfa323b3c385f74d10af625
KAPastor <kyleanthonypastor@gmail.com>
2017-01-09 21:32:53 -0500

    Got the clock functionality working

--- Signal/public/js/signal.js
+++ Signal/public/js/signal.js
@@ -1,11 +1,63 @@
-myAudio = new Audio('/public/sound/waves.wav');
-myAudio.volume = 0.3;
-myAudio.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-}, false);
-myAudio.play();
+// Signal.js: The main event driver for the puzzle
 
+// Start the ominous music in the background.
+  myAudio = new Audio('/public/sound/waves.wav');
+  myAudio.volume = 0.3;
+  myAudio.addEventListener('ended', function() {
+      this.currentTime = 0;
+      this.play();
+  }, false);
+  myAudio.play();
+
+var clockCheckInterval;
+var clockInterval;
+
+// The first thing to do is determine the state of the puzzle by parsing the database:
+  function get_current_state(){
+    // Here we will do an ajax query to get all of the data from the database
+    $.ajax('/current_settings', {
+      success: function(data) {
+        // No we have all of the state data of the puzzle:
+        // If the clock value is greater than 3 run the clock and update the front end
+        if (data.is_clock_on>=3){
+          clearInterval(clockCheckInterval);
+          clockInterval = setInterval('updateClock()', 1000);
+          $('#ClockHeading').html('Looks like the clock has some alarms set.');
+
+        }else{
+          // If this is not the case we run the refresh on the database
+          clockCheckInterval = setInterval('checkClock()', 1000);
+        }
+      },
+      error: function() {
+        console.log('Could not find current state.');
+      }
+    });
+  }
+
+
+// Run the set:
+get_current_state();
+
+
+
+
+
+// Check the clock....
+function checkClock(){
+  $.ajax('/current_settings', {
+    success: function(data) {
+      if (data.is_clock_on>=3){
+        clearInterval(clockCheckInterval);
+        clockInterval = setInterval('updateClock()', 1000);
+        $('#ClockHeading').html('Looks like the clock has some alarms set.');
+      }
+    },
+    error: function() {
+      console.log('Could not update clock.');
+    }
+  });
+}
 
 function updateClock ( )
  	{
@@ -31,7 +83,7 @@ function updateClock ( )
   	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
 
 
-   	$("#clock").html(currentTimeString + '&nbsp;&nbsp;&nbsp; <i class="fa fa-bell" style="cursor:pointer" onClick="OpenAlarm();" data-toggle="modal" data-target="#myModal" aria-hidden="true"></i>');
+   	$("#clock").html(currentTimeString + '&nbsp;&nbsp;&nbsp; <i class="fa fa-bell" style="cursor:pointer"  data-toggle="modal" data-target="#alarmModal" aria-hidden="true"></i>');
 
  };
 
@@ -40,16 +92,3 @@ function scrollTo(divID){
   scrollAmount = lastElementTop;
   $('body').animate({scrollTop: scrollAmount},1000);
 }
-
-
-
-$.ajax('/current_settings', {
-      success: function(data) {
-        // updateClock();
-        if (data.is_clock_on==1){
-          setInterval('updateClock()', 1000);
-        }
-      },
-      error: function() {
-      }
-   });
